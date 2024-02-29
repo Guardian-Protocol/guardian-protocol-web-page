256906b7d36a0dc8b1c2795f6a10f6f4c01ff472
JBunCE <sr.conejo.uwu@gmail.com>
2024-02-28 21:06:55 -0600

    change homepage to /

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Home } from './home';
 import { Main } from './main';
 
-const routes = [{ path: '/', Page: Main },{ path: '/home', Page: Home }];
+const routes = [{ path: '/guardian-protocol-web-page', Page: Main },{ path: '/home', Page: Home }];
 
 function Routing() {
   const getRoutes = () => routes.map(({ path, Page }) => <Route key={path} path={path} element={<Page />} />);
