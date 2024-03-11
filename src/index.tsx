833e2d70d25aadc7291ca604a6569f5016542ba7
JBunCE <sr.conejo.uwu@gmail.com>
2024-03-10 22:35:26 -0600

    Conexion liquidity contract and state request. > > Co-authored-by: C-Chanona carloseduardochanonaaquino@gmail.com

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { App } from "./App";
 
+
 const container = document.getElementById("root");
 const root = createRoot(container as HTMLElement);
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -17,155 +17,101 @@ import {
   Image,
   GridItem,
   Center,
-  Flex,
   Grid,
   Box,
   Text,
 } from "@chakra-ui/react";
-import { useApi, useAlert, useAccount } from "@gear-js/react-hooks";
-import { useState, useEffect } from "react";
+
 import { web3FromSource } from "@polkadot/extension-dapp";
-import { ProgramMetadata, encodeAddress } from "@gear-js/api";
-import VaraLogo from "../../assets/images/VaraLogo.png";
-import XBackground from "../../assets/images/XBackground.svg";
+import { useApi, useAlert, useAccount } from "@gear-js/react-hooks";
+import { getStateMetadata } from "@gear-js/api";
+import { useState, useEffect } from "react";
+import { getLockedBalance } from "contract_utils/GetLockedBalance";
+import { ExtrinsicFactory } from "contract_utils/ExtrinsicFactory";
+import { useWasmMetadata } from "contract_utils/WasmMetadata";
 import { AccountsModal } from "../../components/layout/header/account/accounts-modal/AccountsModal"
+import stateWasm from "../../contract_metadata/state.meta.wasm";
+import XBackground from "../../assets/images/XBackground.svg";
+import VaraLogo from "../../assets/images/VaraLogo.png";
 
 function Home() {
-  useEffect(() => {
-    document.documentElement.style.setProperty("--background-color", "#131111");
-  }, []);
+  const [stakeamount, setStakeamount] = useState<any | undefined>(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [lockedBalance, setLockedBalance] = useState<any | undefined>(0);
+  const [isMessageSuccess, setIsMessageSuccess] = useState(true);
+  const [metaSate, setMetaState] = useState<any>();
+
+  const { api } = useApi();
+  const { buffer } = useWasmMetadata(stateWasm);  
+  const { accounts, account } = useAccount();
 
   const alert = useAlert();
-  const { accounts, account } = useAccount();
-  const { api } = useApi();
-
-  const [gVARAbalance, setGVARAbalance] = useState<any | undefined>(0);
-
-  const [fullState, setFullState] = useState<any | undefined>({});
-
-  const Localbalances = fullState.balances || [];
-
-  // Add your programID
-  const programIDFT =
-    "0xe609506126c6eedd004d964396c52668420916c7f0164af50b40652175ca2641";
-
-  // Add your metadata.txt
-  const metaFT =
-    "00010001000000000001030000000107000000000000000108000000a90b3400081466745f696f28496e6974436f6e66696700000c01106e616d65040118537472696e6700011873796d626f6c040118537472696e67000120646563696d616c73080108753800000400000502000800000503000c081466745f696f204654416374696f6e000118104d696e74040010011075313238000000104275726e040010011075313238000100205472616e736665720c011066726f6d14011c4163746f724964000108746f14011c4163746f724964000118616d6f756e74100110753132380002001c417070726f7665080108746f14011c4163746f724964000118616d6f756e74100110753132380003002c546f74616c537570706c790004002442616c616e63654f66040014011c4163746f724964000500001000000507001410106773746418636f6d6d6f6e287072696d6974697665731c4163746f724964000004001801205b75383b2033325d0000180000032000000008001c081466745f696f1c46544576656e74000110205472616e736665720c011066726f6d14011c4163746f724964000108746f14011c4163746f724964000118616d6f756e74100110753132380000001c417070726f76650c011066726f6d14011c4163746f724964000108746f14011c4163746f724964000118616d6f756e74100110753132380001002c546f74616c537570706c790400100110753132380002001c42616c616e63650400100110753132380003000020081466745f696f3c496f46756e6769626c65546f6b656e00001801106e616d65040118537472696e6700011873796d626f6c040118537472696e67000130746f74616c5f737570706c791001107531323800012062616c616e6365732401505665633c284163746f7249642c2075313238293e000128616c6c6f77616e6365732c01905665633c284163746f7249642c205665633c284163746f7249642c2075313238293e293e000120646563696d616c730801087538000024000002280028000004081410002c00000230003000000408142400";
-
-  const metadataFT = ProgramMetadata.from(metaFT);
-
-  const getBalance = () => {
-    api.programState
-      .read({ programId: programIDFT, payload: "" }, metadataFT)
-      .then((result) => {
-        setFullState(result.toJSON());
-      })
-      .catch(({ message }: Error) => alert.error(message));
-
-    Localbalances.some(([address, balances]: any) => {
-      if (encodeAddress(address) === account?.address) {
-        setGVARAbalance(balances);
-
-        return true;
-      }
-      return false;
-    });
-  };
-
-  useEffect(() => {
-    getBalance();
-  });
-
-  const [stakeamount, setStakeamount] = useState<any | undefined>(0);
-
-  // Add your programID
-  const programID =
-    "0x604379d79c45a4ef9e66d0ea4745e7f413dc58ccda7aa73653ddb1090c22712e";
-
-  // Add your metadata.txt
-  const meta =
-    "00010001000000000001040000000106000000000000000107000000b5072c000808696f18496e69744654000004013466745f70726f6772616d5f696404011c4163746f72496400000410106773746418636f6d6d6f6e287072696d6974697665731c4163746f724964000004000801205b75383b2033325d000008000003200000000c000c0000050300100808696f444c69717569645374616b65416374696f6e000108145374616b6504001401107531323800000020576974686472617704001401107531323800010000140000050700180808696f404c69717569645374616b654576656e740001083c5375636365737366756c5374616b65000000445375636365737366756c556e7374616b65000100001c0808696f34496f4c69717569645374616b6500001c01146f776e657204011c4163746f7249640001547374616b696e675f746f6b656e5f6164647265737304011c4163746f72496400015876617261746f6b656e5f746f74616c5f7374616b65641401107531323800014c746f74616c5f74696d655f70726f746f636f6c20010c7536340001606776617261746f6b656e735f7265776172645f746f74616c14011075313238000144646973747269627574696f6e5f74696d6520010c75363400011475736572732401505665633c284163746f7249642c2075313238293e00002000000506002400000228002800000408041400";
-
-  const programStateMeta = "";
-
-  const metadata = ProgramMetadata.from(meta);
-
-  const unstakemessage = { withdraw: Math.floor(stakeamount) };
-
-  const rewardsemessage = { withdraw: Math.floor(stakeamount) };
-
   const stakemessage = { stake: Math.floor(stakeamount) };
+  const extFactory = new ExtrinsicFactory(accounts, account, api);
+  
+  const stake = async () => {
+    const mintMessageFactory = await extFactory.messageExtrinsic(stakemessage); 
+    const injector = await web3FromSource(accounts[0].meta.source);
 
+    const alertStyle = {
+      style: {
+        color: "white",
+      }
+    }
+    
+    mintMessageFactory?.signAndSend(
+      account?.address ?? alert.error("No account found"),  
+      { signer: injector.signer },
+      ({ status }) => {
+        if (status.isInBlock) {
+          alert.success(status.asInBlock.toHuman()?.toString(), { ...alertStyle });
+        } else if (status.isFinalized) {
+          alert.success(status.type, { ...alertStyle });
+          setIsMessageSuccess(true);
+        } 
+      }
+    )
+  };
+  
   const maxamountvara = () => {
     setStakeamount(account?.balance.value);
   };
-
-  const maxamountgvara = () => {
-    setStakeamount(gVARAbalance);
-  };
-
-  const signer = async (inmessage: any) => {
-    const message: any = {
-      destination: programID, // programId
-      payload: inmessage,
-      gasLimit: 899819245,
-      value: 0,
-    };
-
-    const localaccount = account?.address;
-    const isVisibleAccount = accounts.some(
-      (visibleAccount) => visibleAccount.address === localaccount
-    );
-
-    if (isVisibleAccount) {
-      // Create a message extrinsic
-
-      try {
-        const transferExtrinsic = await api.message.send(message, metadata);
-
-        const injector = await web3FromSource(accounts[0].meta.source);
-        transferExtrinsic
-          .signAndSend(
-            accounts[0].address,
-            { signer: injector.signer },
-            ({ status }: any) => {
-              if (status.isInBlock) {
-                console.log(
-                  `Completed at block hash #${status.asInBlock.toString()}`
-                );
-                alert.success(`Block hash #${status.asInBlock.toString()}`);
-              } else {
-                console.log(`Current status: ${status.type}`);
-                if (status.type === "Finalized") {
-                  alert.success(status.type);
-                  setStakeamount(0);
-                }
-              }
-            }
-          )
-          .catch((error: any) => {
-            console.log(":( transaction failed", error);
-          });
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    }
-  };
-
+  
   const AmountInputChange = async (event: any) => {
     setStakeamount(event.target.value);
   };
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
+  
   const openModal = () => {
     setIsModalOpen(true);
   };
-
+  
   const closeModal = () => {
     setIsModalOpen(false);
   };
+  
+  useEffect(() => {
+    document.documentElement.style.setProperty("--background-color", "#131111");
+    if (buffer !== undefined) {
+      getStateMetadata(buffer as Buffer).then((meta) => {
+        setMetaState(meta);
+      });  
+    }
+  }, [buffer]);
+
+    
+  useEffect(() => {
+    if (buffer !== undefined && api && metaSate && isMessageSuccess) {
+      getLockedBalance(buffer, api, metaSate, account).then((balance) => {
+        try {
+          console.log((balance.toJSON() as { [index: string]: any }).totalLocketBalance.total);
+          setLockedBalance((balance.toJSON() as { [index: string]: any }).totalLocketBalance.total);
+        } catch {
+          setLockedBalance(0);
+        }
+      });
+      setIsMessageSuccess(false);
+    }
+  }, [account, buffer, api, metaSate, isMessageSuccess]);  
 
   return (
     <GridItem
@@ -364,7 +310,7 @@ function Home() {
                         textAlign="end"
                         style={{ color: "white" }}
                       >
-                        4.00%
+                        { parseFloat(account?.balance.value as string) - lockedBalance }
                       </Td>
                     </Grid>
 
@@ -381,7 +327,7 @@ function Home() {
                         textAlign="end"
                         style={{ color: "white" }}
                       >
-                        0.0%
+                        { lockedBalance }
                       </Td>
                     </Grid>
 
@@ -398,7 +344,7 @@ function Home() {
                         textAlign="end"
                         style={{ color: "white" }}
                       >
-                        10%
+                        { account?.balance.value }
                       </Td>
                     </Grid>
 
@@ -417,7 +363,7 @@ function Home() {
                             background: "#F8AD18",
                             width: "240px",
                           }}
-                          onClick={() => signer(stakemessage)}
+                          onClick={stake}
                         >
                           Stake
                         </Button>
@@ -474,7 +420,7 @@ function Home() {
                         textAlign="end"
                         style={{ color: "white" }}
                       >
-                        Available: {gVARAbalance} gVARA
+                        Available: gVARA
                       </Td>
                     </Grid>
 
@@ -636,7 +582,7 @@ function Home() {
                           background: "#F8AD18",
                           width: "240px",
                         }}
-                        onClick={() => signer(unstakemessage)}
+                        onClick={() => {}}
                       >
                         Unstake
                       </Button>
@@ -678,7 +624,7 @@ function Home() {
                         textAlign="end"
                         style={{ color: "white" }}
                       >
-                        Available: {gVARAbalance} gVARA
+                        Available: gVARA
                       </Td>
                     </Grid>
 
@@ -810,7 +756,7 @@ function Home() {
                           background: "#F8AD18",
                           width: "240px",
                         }}
-                        onClick={() => signer(unstakemessage)}
+                        onClick={() => {}}
                       >
                         Withdraw
                       </Button>
