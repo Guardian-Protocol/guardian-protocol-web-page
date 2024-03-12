1b366674c6fcaff583c161c21bd771190383e6f2
JBunCE <sr.conejo.uwu@gmail.com>
2024-03-12 15:47:21 -0600

    gas calculation

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { App } from "./App";
 
-
 const container = document.getElementById("root");
 const root = createRoot(container as HTMLElement);
 
